094bbb75c734ad2fdbe88873d093688eabbad9ac
njomz <njomz@NJOMZA>
2024-02-23 23:30:16 +0100

    test speed of vite

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 export const Home = () => {
   return (
     <div className="home">
-      <h1>Welcome to see the world with us!</h1>
+      <h1>Welcome to see the world with us</h1>
       <h3>What are you in the mood to watch today?</h3>
     </div>
   );
